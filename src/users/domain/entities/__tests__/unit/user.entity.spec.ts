f920310a31605cf3e6d74b50baa81ee1a9ad06ec
Julio Tavares <juliotavares197@gmail.com>
2024-07-13 21:16:20 -0300

    feat: :sparkles: user entity unit test

---
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,24 @@
+import { faker } from '@faker-js/faker'
+import { UserEntity, UserProps } from '../../user.entity'
+
+describe('User Entity', () => {
+  let props: UserProps
+  let sut: UserEntity
+
+  beforeEach(() => {
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    }
+
+    sut = new UserEntity(props)
+  })
+
+  it('Constructor Method', () => {
+    expect(sut.props.name).toEqual(props.name)
+    expect(sut.props.email).toEqual(props.email)
+    expect(sut.props.password).toEqual(props.password)
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
+})
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class User {}
+export type UserProps = {
+  name: string
+  email: string
+  password: string
+  createdAt?: Date
+}
+
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date()
+  }
+}
