f1217093551517654c60514b77a038947c4b19c8
Julio Tavares <juliotavares197@gmail.com>
2024-07-13 21:24:26 -0300

    refactor: :recycle: refactor entity methods

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -21,4 +21,27 @@ describe('User Entity', () => {
     expect(sut.props.password).toEqual(props.password)
     expect(sut.props.createdAt).toBeInstanceOf(Date)
   })
+
+  it('should return name field', () => {
+    expect(sut.name).toBeDefined()
+    expect(sut.name).toEqual(props.name)
+    expect(typeof sut.name).toBe('string')
+  })
+
+  it('should return email field', () => {
+    expect(sut.email).toBeDefined()
+    expect(sut.email).toEqual(props.email)
+    expect(typeof sut.email).toBe('string')
+  })
+
+  it('should return password field', () => {
+    expect(sut.password).toBeDefined()
+    expect(sut.password).toEqual(props.password)
+    expect(typeof sut.password).toBe('string')
+  })
+
+  it('should return createdAt field', () => {
+    expect(sut.createdAt).toBeDefined()
+    expect(sut.createdAt).toBeInstanceOf(Date)
+  })
 })
