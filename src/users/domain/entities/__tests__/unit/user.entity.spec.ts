b756eab51a17b617f1e7073b4ecd49f3e26569d9
Julio Tavares <juliotavares197@gmail.com>
2024-07-13 21:34:22 -0300

    refactor: :recycle: create testing helper

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,16 +1,13 @@
 import { faker } from '@faker-js/faker'
 import { UserEntity, UserProps } from '../../user.entity'
+import { userDataBuilder } from '../../testing/helpers/user-data-builder'
 
 describe('User Entity', () => {
   let props: UserProps
   let sut: UserEntity
 
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    }
+    props = userDataBuilder({})
 
     sut = new UserEntity(props)
   })
