5115f72aec42e83843d946736224aae4f3e61503
Julio Tavares <juliotavares197@gmail.com>
2024-07-15 14:46:35 -0300

    refactor: :recycle: create intragtion test config

---
+++ vitest.config.integration.ts
@@ -0,0 +1,17 @@
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  test: {
+    include: ['**/*.int-spec.ts'],
+    globals: true,
+    root: './',
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+})
